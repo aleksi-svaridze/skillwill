93f8983cedb771fa1ca72a18a28076b595ca3096
aleksi-svaridze <aleksi.svaridze82@gmail.com>
2025-11-12 12:42:27 +0400

    Fix: first task

---
+++ task-4/src/main.js
@@ -0,0 +1,49 @@
+import "./style.css";
+
+// შექმენი <button> და <div> ელემენტები და მიანიჭეთ საკუთარი უნიკალური id
+// ● <div> - ში ჩაწერე პატარა random ტექსტი
+// ● ღილაკზე დაჭერის შემთხვევაში დამალე <div> ელემენტი
+
+const btn = document.createElement("button");
+const div = document.createElement("div");
+const buttonText = document.createTextNode("Click me!");
+const divText = document.createTextNode(
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+);
+
+div.style.width = "400px";
+div.style.borderRadius = "10px";
+div.style.padding = "10px 20px";
+div.style.border = "2px solid orange";
+div.setAttribute("id", "orangeDiv");
+div.appendChild(divText);
+
+btn.appendChild(buttonText);
+btn.setAttribute("id", "button");
+btn.style.marginBottom = "20px";
+
+btn.addEventListener("click", () => {
+  div.style.display = "none";
+});
+
+document.body.prepend(btn, div);
+
+// შექმენი შემდეგი სტრუქტურა JS -ის
+// გამოყენებით და დაამატე DOM-ში:
+// <div id=”card”>
+// <h2>Gandalf</h2>
+// <a href=”#”>Go to profile</a>
+// </div>
+
+// შექმენი quiz თამაში
+// ● დაწერე რამდენიმე მოკლე შეკითხვა
+// თავისი სავარაუდო პასუხებით
+// სწორ პასუხზე დაჭერის შემთხვევაში
+// გაამწვანე პასუხი
+// ● არასწორი პასუხის შემთხვევაში გააწითლე
+
+//  ბონუს დავალება:
+// გამოიტანე ეკრანზე ქულების რაოდენობა
+// ➔ სწორი პასუხის შემთხვევაში დაამატე 1
+// ქულა
+// ➔ არასწორის შემთხვევაში უცვლელი რჩება
