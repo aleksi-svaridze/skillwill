c4f2ebb882f5da185b10e5af8604b1320a7c3ad5
aleksi-svaridze <aleksi.svaridze82@gmail.com>
2025-11-26 17:46:39 +0400

    Add task definition to index.js file

--- 9-npm-node/index.js
+++ 9-npm-node/index.js
@@ -2,3 +2,18 @@ import validator from "validator";
 
 console.log(validator.isEmail("abcDE123")); // False
 console.log(validator.isEmail("test@test.com")); // True
+
+// 1 . მოახდინე პროექტის ინიციალიზაცია
+// npm init
+
+//  2. დააინსტალირე პაკეტი validator
+// npm i validator
+
+//  3. მოიძიე validator-ის დოკუმენტაცია
+// https://www.npmjs.com/package/validator
+
+// 4 . დოკუმენტაციის მიხედვით გამოიყენე ბრძანება isEmail და დაბეჭდე არის თუ არა
+//     შემდეგი სიტყვები მეილი: test@test.com, abcDE123
+
+// console.log(validator.isEmail("abcDE123")); // False
+// console.log(validator.isEmail("test@test.com")); // True
